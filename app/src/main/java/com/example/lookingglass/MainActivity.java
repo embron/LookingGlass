0605597c116900787abf5e065e923d1060c45081
Tex Starshine <60279934+AlexanderVikingWest@users.noreply.github.com>
2021-07-05 19:59:55 -0500

    roll call

--- app/src/main/java/com/example/lookingglass/MainActivity.java
+++ app/src/main/java/com/example/lookingglass/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+//project member names here:
+// Alexander West
+
 public class MainActivity extends AppCompatActivity {
 
     @Override

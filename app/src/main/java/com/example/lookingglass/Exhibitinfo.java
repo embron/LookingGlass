884e47a31a92674b0b60217c6fb394f806b820d8
Tex Starshine <60279934+AlexanderVikingWest@users.noreply.github.com>
2021-08-12 16:30:19 -0500

    string formatting major bugfix
    tadah

--- app/src/main/java/com/example/lookingglass/Exhibitinfo.java
+++ app/src/main/java/com/example/lookingglass/Exhibitinfo.java
@@ -4,8 +4,14 @@ import android.app.Application;
 
 public class Exhibitinfo extends Application{
     public String Qrcode;
-    public String[] history = new String[10];
+    public String[] history;
     public int count=0;
 
+    public Exhibitinfo()
+    {
+        Qrcode = "error";
+        history = new String[10];
+        count = 0;
+    }
 
 }
